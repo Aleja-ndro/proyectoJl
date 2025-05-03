ca6a94c903fd736dbcf9fcb198c4fa297ac114f4
First Last <omaralemayorga@gmail.com>
2025-05-03 14:37:35 -0300

    responsivo

--- vite-project/src/pages/Prueba.jsx
+++ vite-project/src/pages/Prueba.jsx
@@ -347,53 +347,59 @@ const handleModificarProducto = async (datosActualizados) => {
         <div className="flex-1">
           <InputBusqueda busqueda={busqueda} setBusqueda={setBusqueda} />
         </div>
-        <div className="flex gap-2">
-          <button
-            onClick={() => setShowAddModal(true)}
-            className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg flex items-center gap-2"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
-            </svg>
-            Agregar
-          </button>
-          <button
-            onClick={() => productoSeleccionado ? setShowEditModal(true) : alert("Selecciona un producto primero")}
-            disabled={!productoSeleccionado}
-            className={`px-4 py-2 rounded-lg flex items-center gap-2 ${
-              !productoSeleccionado 
-                ? 'bg-gray-500 cursor-not-allowed' 
-                : 'bg-yellow-600 hover:bg-yellow-700 text-white'
-            }`}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-            </svg>
-            Modificar
-          </button>
-          <button
-            onClick={handleEliminarProducto}
-            disabled={!productoSeleccionado}
-            className={`px-4 py-2 rounded-lg flex items-center gap-2 ${
-              !productoSeleccionado 
-                ? 'bg-gray-500 cursor-not-allowed' 
-                : 'bg-red-600 hover:bg-red-700 text-black'
-            }`}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-            </svg>
-            Eliminar
-          </button>
-          <button
-            onClick={handleLogout}
-            className="px-4 py-2 rounded-lg flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" />
-            </svg>
-            Salir
-          </button>
+        <div className="flex flex-wrap gap-2 sm:flex-nowrap">
+        <button
+    onClick={() => setShowAddModal(true)}
+    className="flex-1 min-w-[120px] sm:flex-none bg-green-600 hover:bg-green-700 text-white px-3 py-1.5 sm:px-4 sm:py-2 rounded-lg flex items-center justify-center gap-1 sm:gap-2 text-sm sm:text-base transition-colors"
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5" viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
+    </svg>
+    <span>Agregar</span>
+  </button>
+
+  {/* Botón Modificar */}
+  <button
+    onClick={() => productoSeleccionado ? setShowEditModal(true) : alert("Selecciona un producto primero")}
+    disabled={!productoSeleccionado}
+    className={`flex-1 min-w-[120px] sm:flex-none px-3 py-1.5 sm:px-4 sm:py-2 rounded-lg flex items-center justify-center gap-1 sm:gap-2 text-sm sm:text-base transition-colors ${
+      !productoSeleccionado 
+        ? 'bg-gray-500 cursor-not-allowed' 
+        : 'bg-yellow-600 hover:bg-yellow-700 text-white'
+    }`}
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5" viewBox="0 0 20 20" fill="currentColor">
+      <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+    </svg>
+    <span>Modificar</span>
+  </button>
+
+  {/* Botón Eliminar */}
+  <button
+    onClick={handleEliminarProducto}
+    disabled={!productoSeleccionado}
+    className={`flex-1 min-w-[120px] sm:flex-none px-3 py-1.5 sm:px-4 sm:py-2 rounded-lg flex items-center justify-center gap-1 sm:gap-2 text-sm sm:text-base transition-colors ${
+      !productoSeleccionado 
+        ? 'bg-gray-500 cursor-not-allowed' 
+        : 'bg-red-600 hover:bg-red-700 text-white'
+    }`}
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5" viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+    </svg>
+    <span>Eliminar</span>
+  </button>
+
+  {/* Botón Salir */}
+  <button
+    onClick={handleLogout}
+    className="flex-1 min-w-[120px] sm:flex-none bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 sm:px-4 sm:py-2 rounded-lg flex items-center justify-center gap-1 sm:gap-2 text-sm sm:text-base transition-colors"
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5" viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" />
+    </svg>
+    <span>Salir</span>
+  </button>
         </div>
       </div>
 
